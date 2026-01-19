66b8d69c495d5a6cd01666c6152e40d8dea0e14d
MIMminE <weluv.ww@gmail.com>
2026-01-19 16:56:27 +0900

    initialize commerce project with Gradle and Spring Boot setup

---
+++ src/test/kotlin/nuts/project/commerce/CommerceApplicationTests.kt
@@ -0,0 +1,13 @@
+package nuts.project.commerce
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommerceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
